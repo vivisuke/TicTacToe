9d8a640acce677260a45f531d97bcfbbd5b32e68
vivisuke <ntsuda@master.email.ne.jp>
2021-10-22 09:50:18 +0900

    0012 ◎ C 10/22 10/22 0.001│ToDo│Q値表示用ラベル設置

--- TicTacToe/MainScene.gd
+++ TicTacToe/MainScene.gd
@@ -19,12 +19,20 @@ var next				# TILE_O or TILE_X
 var bits_O = 0			# O 状態ビットボード
 var bits_X = 0			# X 状態ビットボード
 var is_victory_table = []		# ビットボード → ３目並びがあるか？
+var Q = []				# Q値テーブル
+var qvalLabel = []		# Q値表示用ラベル
+var rng = RandomNumberGenerator.new()
+
+var QValLabel = load("res://QValLabel.tscn")
 
 func _ready():
+	Q.resize(N_3_POWER_9)		# 全状態：3^9
 	build_victory_table()
 	init_board()
-	update_nextLabel()
-	pass # Replace with function body.
+	init_qvlabels()			# Q値表示用ラベル初期化
+	update_qvLabels()		# Q値表示
+	update_nextLabel()		# 次の手番ラベル更新
+	pass
 func init_board():
 	ended = false
 	next = TILE_O
@@ -34,6 +42,49 @@ func init_board():
 	for y in N_VERT:
 		for x in N_HORZ:
 			$Board/TileMap.set_cell(x, y, TILE_EMPTY)
+func init_qvlabels():
+	for y in range(N_VERT):
+		for x in range(N_HORZ):
+			var label = QValLabel.instance()
+			label.rect_position = Vector2(x*CELL_WIDTH+4, y*CELL_WIDTH) + $Board.rect_position
+			add_child(label)
+			qvalLabel.push_back(label)
+func make_Q(qix):
+	print("make_Q(%d)" % qix)
+	if Q[qix] != null: return
+	Q[qix] = []
+	var empty = ~(bits_O | bits_X)		# 1 for EMPTY
+	var mask = 1
+	for i in range(9):
+		if next == TILE_O:		# O の手番
+			Q[qix].push_back(1.0 if (empty&mask) != 0 else -9.999)
+		else:					# X の手番
+			Q[qix].push_back(-1.0 if (empty&mask) != 0 else 9.999)
+		mask <<= 1
+func update_qvLabels():		# 現状態の各行動のQ値を表示
+	var qix = bb_to_qix(bits_O, bits_X)
+	make_Q(qix)
+	var mask = 1
+	var empty = ~(bits_O | bits_X)		# 1 for EMPTY
+	for i in range(9):
+		if (empty & mask) == 0:
+			qvalLabel[i].text = ""
+		else:
+			qvalLabel[i].text = "%.3f" % Q[qix][i]
+		mask <<= 1
+func bb_to_qix(bO, bX):
+	var qix = 0
+	var mask = 1 << 8
+	while mask != 0:
+		qix *= 3
+		if (bO & mask) != 0:
+			qix += 1
+		elif (bX & mask) != 0:
+			qix += 2
+		mask >>= 1
+	return qix
+func ix_to_xy(ix : int):
+	return [ix % N_HORZ, ix / N_HORZ]
 func set_cell(x : int, y : int, v : int):
 	$Board/TileMap.set_cell(x, y, v)
 	var mask = 1 << (y*N_HORZ + x)
@@ -78,7 +129,8 @@ func _input(event):
 			if map.x < 0 || map.x >= N_HORZ || map.y < 0 || map.y >= N_VERT: return
 			set_cell(map.x, map.y, next)
 			next = (TILE_O + TILE_X) - next			# 手番交代
-		update_nextLabel()
+		update_qvLabels()		# Q値表示
+		update_nextLabel()		# 次の手番ラベル更新
 		
 func _process(delta):
 	pass
