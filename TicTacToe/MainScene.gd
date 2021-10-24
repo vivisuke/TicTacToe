16c40ebe31366b89eef88e7b06d66d87e51b59ba
vivisuke <ntsuda@master.email.ne.jp>
2021-10-24 09:02:34 +0900

    0014 ◎ C 10/23 10/23 0.001│ToDo│AI vs Rand * 100 （非学習）

--- TicTacToe/MainScene.gd
+++ TicTacToe/MainScene.gd
@@ -16,6 +16,7 @@ enum {
 enum {
 	MODE_NONE = 0,
 	MODE_RAND_RAND,
+	MODE_AI_RAND,		# AI vs Rand 非学習
 	MODE_AI_AI,			# AI 自己対戦学習
 }
 var mode = MODE_NONE
@@ -23,6 +24,8 @@ var nEpisode = 0
 var nEpisodeRest = 0
 var nOwon = 0			# O 勝数
 var nXwon = 0			# X 勝数
+var nAIwon = 0			# AI 勝数
+var nRANDwon = 0			# RAND 勝数
 var nDraw = 0			# 引き分け数
 var started = false
 var ended = false		# 終局状態
@@ -180,7 +183,7 @@ func modeRandRand():
 	nEpisodeRest -= 1
 	if !nEpisodeRest:
 		mode = MODE_NONE
-		print("nOwon = %d (%.1f%%)" % [nOwon, nOwon * 100.0/nEpisode])
+		print("\nnOwon = %d (%.1f%%)" % [nOwon, nOwon * 100.0/nEpisode])
 		print("nXwon = %d (%.1f%%)" % [nXwon, nXwon * 100.0/nEpisode])
 		print("nDraw = %d (%.1f%%)" % [nDraw, nDraw * 100.0/nEpisode])
 func nextMove(qix):
@@ -212,6 +215,67 @@ func nextMove(qix):
 		return lst[0]
 	else:
 		return lst[rng.randi_range(0, lst.size() - 1)]
+func modeAiRand():		# AI vs RAND、学習あり
+	nEpisode += 1
+	# nEpisode が奇数の場合は AI が先手（O）、偶数であれば AIが後手（X）
+	$EpiNumLabel.text = "#%d" % nEpisode
+	init_board()
+	var AIturn : bool = (nEpisode & 1) != 0
+	while true:
+		var qix = bb_to_qix(bits_O, bits_X)
+		make_Q(qix)
+		var ix
+		if AIturn:
+			ix = nextMove(qix)
+		else:
+			var lst = get_empty_list()
+			ix = lst[rng.randi_range(0, lst.size() - 1)]
+		var t = ix_to_xy(ix)
+		set_cell(t[0], t[1], next)
+		next = (TILE_O + TILE_X) - next			# 手番交代
+		AIturn = !AIturn
+		var reward = 0.0
+		var ef = false
+		if is_victory_table[bits_O]:
+			#nOwon += 1
+			if (nEpisode & 1) != 0:		# AI が先手（O）
+				nAIwon += 1
+			else:
+				nRANDwon += 1
+			reward = 1.0
+			ef = true
+			#break;
+		elif is_victory_table[bits_X]:
+			#nXwon += 1
+			if (nEpisode & 1) == 0:		# AI が後手（X）
+				nAIwon += 1
+			else:
+				nRANDwon += 1
+			reward = -1.0
+			ef = true
+			#break;
+		elif is_draw():
+			nDraw += 1
+			ef = true
+			#break;
+		var qix2 = bb_to_qix(bits_O, bits_X)
+		make_Q(qix2)
+		var minmaxQ = 0.0
+		if !ef:
+			if next == TILE_O:
+				minmaxQ = Q[qix2].max()
+			else:
+				minmaxQ = Q[qix2].min()
+		Q[qix][ix] += ALPHA * (reward + GAMMA * minmaxQ - Q[qix][ix])
+		if ef: break
+	update_nextLabel()
+	update_qvLabels()
+	nEpisodeRest -= 1
+	if !nEpisodeRest:
+		mode = MODE_NONE
+		print("\nnAIwon = %d (%.1f%%)" % [nAIwon, nAIwon * 100.0/nEpisode])
+		print("nRANDwon = %d (%.1f%%)" % [nRANDwon, nRANDwon * 100.0/nEpisode])
+		print("nDraw = %d (%.1f%%)" % [nDraw, nDraw * 100.0/nEpisode])
 func modeAiAi():
 	nEpisode += 1
 	$EpiNumLabel.text = "#%d" % nEpisode
@@ -251,12 +315,14 @@ func modeAiAi():
 	nEpisodeRest -= 1
 	if !nEpisodeRest:
 		mode = MODE_NONE
-		print("nOwon = %d (%.1f%%)" % [nOwon, nOwon * 100.0/nEpisode])
+		print("\nnOwon = %d (%.1f%%)" % [nOwon, nOwon * 100.0/nEpisode])
 		print("nXwon = %d (%.1f%%)" % [nXwon, nXwon * 100.0/nEpisode])
 		print("nDraw = %d (%.1f%%)" % [nDraw, nDraw * 100.0/nEpisode])
 func _process(delta):
 	if mode == MODE_RAND_RAND:
 		modeRandRand()
+	elif mode == MODE_AI_RAND:
+		modeAiRand()
 	elif mode == MODE_AI_AI:
 		modeAiAi()
 	pass
@@ -279,4 +345,14 @@ func _on_AIxAIx100Button_pressed():
 	nXwon = 0
 	nDraw = 0
 	mode = MODE_AI_AI
+	pass # Replace with function body.
+
+
+func _on_AIxRx100Button_pressed():
+	nEpisode = 0
+	nEpisodeRest = 100
+	nAIwon = 0
+	nRANDwon = 0
+	nDraw = 0
+	mode = MODE_AI_RAND
 	pass # Replace with function body.
